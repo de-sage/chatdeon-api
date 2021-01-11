03ef94c8a293c961a49e464668cc64d70c4727b8
de-sage <gomotosho1@gmail.com>
2021-01-12 00:06:53 +0100

    adding dto mapper annotations

--- src/main/java/com/social_media/blog/util/DTOModelMapper.java
+++ src/main/java/com/social_media/blog/util/DTOModelMapper.java
@@ -3,7 +3,11 @@ package com.social_media.blog.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -11,6 +15,10 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 import javax.persistence.EntityManager;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.rmi.RemoteException;
 import java.util.Collections;
 
 public class DTOModelMapper extends RequestResponseBodyMethodProcessor {
@@ -35,6 +43,27 @@ public class DTOModelMapper extends RequestResponseBodyMethodProcessor {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        return super.resolveArgument(parameter, mavContainer, webRequest, binderFactory);
+       Object dto =  super.resolveArgument(parameter, mavContainer, webRequest, binderFactory);
+       Object id = getEntityId(dto);
+
+       if(id == null) {
+           return modelMapper.map(dto, parameter.getParameterType());
+       } else {
+           Object persistedObject = entityManager.find(parameter.getParameterType(), id);
+           modelMapper.map(dto, persistedObject);
+           return persistedObject;
+       }
     }
+
+    @Override
+    protected Object readWithMessageConverters(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType) throws IOException, HttpMediaTypeNotSupportedException, HttpMessageNotReadableException {
+        for (Annotation ann : parameter.getParameterAnnotations()) {
+            DTO dtoType = AnnotationUtils.getAnnotation(ann, DTO.class);
+            if (dtoType != null) {
+                return super.readWithMessageConverters(inputMessage, parameter, dtoType.value());
+            }
+        }
+        throw new RemoteException();
+    }
+
 }
